4c481011d95875cff6a4a0291e3092f0514c2a29
saravanan510 <saravanan5101998@gmail.com>
2025-07-20 18:35:03 +0530

    navbar fixes

--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -1,18 +1,32 @@
 "use client";
 import Image from "next/image";
 import Button from "../common/button/Button";
-import { useState } from "react";
+import { useState, useEffect } from "react"; // Import useEffect
+
 const Header = () => {
   const [open, setOpen] = useState(false);
+
   const handleMenu = () => {
     setOpen((prev) => !prev);
   };
+
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, [open]);
+
   return (
     <>
-      <div className="container px-6 mx-auto 2xl:px-20 h-20 flex justify-between items-center border-b border-b-gray-200">
+      <div className="container relative px-6 mx-auto 2xl:px-20 h-20 flex justify-between items-center border-b border-b-gray-200">
         <Image src="/thedigiseedlogo.svg" alt="logo" width={120} height={32} />
 
-        <div className="flex items-center gap-8">
+        <div className="flex items-center gap-8 ">
           <ul className="hidden lg:flex gap-8">
             <li>About Us</li>
             <li>Services</li>
@@ -44,12 +58,12 @@ const Header = () => {
       </div>
       {/* Menu slider */}
       {open && (
-        <div className="bg-[#f4f4f4] h-dvh">
+        <div className="bg-[#f4f4f4] h-dvh w-full top-20 absolute z-50">
           <div className="container mx-auto p-6">
             <ul className="mb-6">
-              <li className="py-4 border-b-1 border-gray-200">About Us</li>
-              <li className="py-4 border-b-1 border-gray-200">Services</li>
-              <li className="py-4 border-b-1 border-gray-200">Works</li>
+              <li className="py-4 border-b border-gray-200">About Us</li>
+              <li className="py-4 border-b border-gray-200">Services</li>
+              <li className="py-4 border-b border-gray-200">Works</li>
             </ul>
             <div className="">
               <Button type={"contained"}>Become a Client</Button>
